a1c8e1c0459e0b3d61dac5d7ac9fd58e8b79fa99
Edisson Reinozo <edisson.reinozo@ucuenca.edu.ec>
2020-07-04 18:08:33 -0500

    SearchBox functionality

---
+++ src/common/constants.ts
@@ -0,0 +1,4 @@
+export const ENTER_KEY = 13;
+
+export const BASE_VOICE_SEARCH_ENDPOINT =
+  'https://api.sandbox.voice123.com/providers/search/';
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,51 +1,82 @@
 import React from 'react';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/styles';
 import { Paper, IconButton, InputBase } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
+import { ENTER_KEY } from '../common/constants';
 
 export interface SearchProps {
+  classes: any;
   placeholder: string;
-  value?: string;
+  onSearchClicked: (searchTerm: string) => void;
 }
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      padding: '2px 4px',
-      display: 'flex',
-      alignItems: 'center',
-      width: 600,
-      marginBottom: 10,
-    },
-    input: {
-      marginLeft: theme.spacing(1),
-      flex: 1,
-    },
-    iconButton: {
-      padding: 10,
-    },
-  })
-);
-
-function Search(props: SearchProps) {
-  const classes = useStyles();
-
-  return (
-    <Paper component='form' className={classes.root}>
-      <InputBase
-        className={classes.input}
-        placeholder={props.placeholder}
-        value={props.value}
-      />
-      <IconButton
-        type='submit'
-        className={classes.iconButton}
-        aria-label='search'
-      >
-        <SearchIcon />
-      </IconButton>
-    </Paper>
-  );
+export interface SearchState {
+  searchTerm: string;
 }
 
-export default Search;
+const useStyles = {
+  root: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    width: 600,
+    marginBottom: 10,
+  },
+  input: {
+    marginLeft: 10,
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+};
+
+class Search extends React.Component<SearchProps, SearchState> {
+  constructor(props: SearchProps) {
+    super(props);
+
+    this.state = {
+      searchTerm: '',
+    };
+  }
+
+  onSearchTermUpdate = (value: string) => {
+    this.setState({
+      searchTerm: value,
+    });
+  };
+
+  keyPress = (e: any) => {
+    if (e.keyCode === ENTER_KEY) {
+      this.props.onSearchClicked(e.target.value);
+    }
+  };
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <Paper className={classes.root}>
+        <InputBase
+          className={classes.input}
+          placeholder={this.props.placeholder}
+          value={this.state.searchTerm}
+          onChange={(e) => this.onSearchTermUpdate(e.target.value)}
+          onKeyDown={this.keyPress}
+        />
+        <IconButton
+          className={classes.iconButton}
+          aria-label='search'
+          onClick={() => this.props.onSearchClicked(this.state.searchTerm)}
+          onSubmit={() => {}}
+        >
+          <SearchIcon />
+        </IconButton>
+      </Paper>
+    );
+  }
+}
+
+// export default Search;
+
+export default withStyles(useStyles)(Search);
